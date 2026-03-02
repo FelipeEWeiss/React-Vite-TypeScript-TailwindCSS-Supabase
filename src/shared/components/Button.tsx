0fca349e96fbfcda31a521a83bcde3038dcc6a8a
Felipe <felipeew19@gmail.com>
2026-03-02 17:24:53 +0000

    feat{button): created a shared button component

---
+++ src/shared/components/Button.tsx
@@ -0,0 +1,35 @@
+interface ButtonProps {
+  text?: string;
+  handleClick?: () => null;
+  variant?: "primary" | "secondary";
+  buttonType?: "submit" | "button" | "reset";
+  children?: any;
+}
+
+export const Button = ({
+  text = "Hello",
+  handleClick,
+  variant = "primary",
+  buttonType = "button",
+  children,
+}: ButtonProps) => {
+  const baseStyle =
+    "cursor-pointer rounded-xl py-4 w-full text-sm font-semibold transition flex justify-center items-center gap-2";
+
+  const variants = {
+    primary: "bg-blue-600 text-white hover:bg-blue-700",
+    secondary:
+      "border border-gray-300 text-gray-600 hover:opacity-50 duration-200",
+  };
+
+  return (
+    <button
+      type={buttonType}
+      onClick={handleClick}
+      className={`${baseStyle} ${variants[variant]}`}
+    >
+      {children}
+      {text}
+    </button>
+  );
+};
